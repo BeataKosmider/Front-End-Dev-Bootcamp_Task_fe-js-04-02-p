78bb10ae04d7ca2a7c052382eeedeec54fe2762e
Beata Kosmider <kosmibe@gmail.com>
2023-02-03 15:19:25 +0100

    task is done

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const createList = () => {
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "list");
+  document.body.appendChild(ul);
+
+  ["Item 1", "Item 2", "Item 3"].map((item) => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    ul.appendChild(li);
+  });
+  return ul;
   // Zaimplementuj zadanie zgodnie z wytycznymi
 };
 
 module.exports = createList;
+/*Stwórz funkcję createList, która będzie tworzyła 3 elementową listę zakupów. Lista powinna mieć postać znacznika <ul>, posiadać identyfikator (id) list, a zawartość elementów <li> może być dowolna (jakikolwiek tekst). W zadaniu należy użyć funkcji document.createElement do tworzenia elementów i document.body.appendChild do podpięcia listy do elementu <body>. Funkcja musi również zwrócić (return) samą listę.
+
+notka: Do stworzenia 3-elementowej listy możesz użyć takiej techniki:
+
+["Item 1", "Item 2", "Item 3"].map(item => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  return li;
+});
+
+Przykładowy input:
+
+brak
+
+Przykładowy output:
+
+Element <ul id="list"><li>Test 1</li><li>Test 2</li><li>Test 3</li></ul>*/
